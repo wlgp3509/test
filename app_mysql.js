2a7b220cf84759210843050ba7865aa638bf7875
hataerin <goil1113@likelion.org>
2019-02-09 17:42:33 +0900

    update host

--- app_mysql.js
+++ app_mysql.js
@@ -13,7 +13,7 @@ var upload = multer({ storage: _storage })
 var fs = require('fs');
 var mysql = require('mysql');
 var conn = mysql.createConnection({
-  host     : 'localhost',
+  host     : '13.124.33.78'
   user     : 'root',
   password : '1111',
   database : 'taerin'
